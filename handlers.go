cb7bf045a1ca18455bc2661973ae7f881cfde5ea
SaschaRunge <sascharunge89@yahoo.de>
2026-04-01 19:25:58 +0200

    lesson "Routing"

--- handlers.go
+++ handlers.go
@@ -4,8 +4,8 @@ import (
 	"net/http"
 )
 
-func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
--- main.go
+++ main.go
@@ -3,12 +3,44 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerReturnFileServerHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	s := fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())
+	w.Write([]byte(s))
+}
+
+func (cfg *apiConfig) handlerResetFileServerHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileServerHits.Store(0)
+	w.Write([]byte("Reset FileServerHits counter to 0."))
+}
+
 func main() {
+	cfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle(
+		"/app/",
+		http.StripPrefix("/app/", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))),
+	)
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", cfg.handlerReturnFileServerHits)
+	mux.HandleFunc("POST /reset", cfg.handlerResetFileServerHits)
 	server := http.Server{
 		Addr:                         ":8080",
 		Handler:                      mux,
